327356090e82d549704dbfe87e1d851d27e5b620
AntwortEinesLebens <AntwortEinesLebens@proton.me>
2026-03-20 09:35:29 +0100

    feat: ✨ Add provider parser

--- src/laboratories/laboratory.rs
+++ src/laboratories/laboratory.rs
@@ -40,10 +40,44 @@ impl<'de> Deserialize<'de> for Network {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Provider {
+    Kvm,
+}
+
+impl Provider {
+    const EXPECTED_VALUES: &'static [&'static str] = &["kvm"];
+
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Self::Kvm => "kvm",
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for Provider {
+    fn deserialize(value: &mut Value<'de>) -> Result<Self, DeserError> {
+        let string_value = value.take_string(None)?;
+        match string_value.as_ref() {
+            "kvm" => Ok(Self::Kvm),
+            _ => Err(Error {
+                kind: ErrorKind::UnexpectedValue {
+                    expected: Self::EXPECTED_VALUES,
+                    value: Some(string_value.into_owned()),
+                },
+                span: value.span,
+                line_info: None,
+            }
+            .into()),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Laboratory {
     pub name: String,
     pub network: Network,
+    pub provider: Provider,
 }
 
 impl<'de> Deserialize<'de> for Laboratory {
@@ -51,8 +85,13 @@ impl<'de> Deserialize<'de> for Laboratory {
         let mut table = TableHelper::new(value)?;
         let name = table.required("name")?;
         let network = table.required("network")?;
+        let provider = table.required("provider")?;
         table.finalize(None)?;
 
-        Ok(Self { name, network })
+        Ok(Self {
+            name,
+            network,
+            provider,
+        })
     }
 }
